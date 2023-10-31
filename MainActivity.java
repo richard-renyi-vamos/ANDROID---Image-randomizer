db29f3b52db97faa0223f32824a83e8951338654
Richard Renyi-Vamos <82152266+richard-renyi-vamos@users.noreply.github.com>
2023-10-31 17:09:35 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,14 @@
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-
+import android.widget.Button;
+import android.view.View;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    Button randomizeButton;
     int[] imageArray = {R.drawable.image1, R.drawable.image2, R.drawable.image3}; // Add your image resources here
 
     @Override
@@ -15,10 +17,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.imageView); // Make sure to have an ImageView in your layout with the id "imageView"
+        randomizeButton = findViewById(R.id.randomizeButton); // Get the reference to the randomize button
         
-        // Implement logic to show a random image
-        Random random = new Random();
-        int randomIndex = random.nextInt(imageArray.length);
-        imageView.setImageResource(imageArray[randomIndex]);
+        randomizeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Implement logic to show a random image
+                Random random = new Random();
+                int randomIndex = random.nextInt(imageArray.length);
+                imageView.setImageResource(imageArray[randomIndex]);
+            }
+        });
     }
 }
